36a17715f14b65367bac2d79c866c04aec73542a
Vincent Desmares <vincent.desmares@gmail.com>
2016-10-02 17:52:09 -0700

    * Start entity page

--- src/routes/Entity/components/EntityView.js
+++ src/routes/Entity/components/EntityView.js
@@ -5,36 +5,105 @@ import FontIcon from 'material-ui/FontIcon';
 import classes from './EntityView.scss'
 
 import paletteClass from 'material-design-lite/src/palette/_palette.scss';
-
+import Subheader from 'material-ui/Subheader';
 import classNames from 'classnames';
-
+import IconButton from 'material-ui/IconButton';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 const { Grid, Row, Col } = require('react-flexbox-grid');
 
-let tmpClasses = {'material-icons': true};
-tmpClasses[classes.linkCardMmaterialIcon] = true;
+import { GridList, GridTile } from 'material-ui/GridList';
 
-let jobClass = {'material-icons': true, 'mdl-color-text--grey-600': true};
-jobClass[classes.linkCardMmaterialIcon] = true;
+let tmpClasses = { 'material-icons': true };
+tmpClasses[ classes.linkCardMmaterialIcon ] = true;
+
+let jobClass = { 'material-icons': true, 'mdl-color-text--grey-600': true };
+jobClass[ classes.linkCardMmaterialIcon ] = true;
 
 jobClass = classNames(jobClass);
 
-import {blue400, red400, green400, orange400, purple400, yellow400} from 'material-ui/styles/colors';
+import { blue400, red400, green400, orange400, purple400, yellow400 } from 'material-ui/styles/colors';
 
 var btnClass = classNames(tmpClasses);
-export const EntityView = () => (
+export const EntityView = (props) => (
     <div>
-        <Grid>
-            <Row className={'mdl-color-text--grey-800'}>
-                <Col md={2}>
-                    <Card className={classes.linkCard}>
-                        <CardMedia style={{backgroundColor: blue400}}>
-                            <FontIcon className={btnClass}>layers</FontIcon>
-                        </CardMedia>
-                        <CardTitle title="Scenes" subtitle="12 configurations"/>
-                    </Card>
-                </Col>
-            </Row>
-        </Grid>
+        {(() => {
+            if (!props.children) {
+                return <div>
+                    <GridList
+                    cellHeight={200}
+                    cols={6}
+                >
+                    <Subheader>Most Recent Edited</Subheader>
+                    <GridTile
+                        key={"1"}
+                        title={"Cruiser Amarr 1"}
+                        subtitle={<span>type <b>spaceship</b></span>}
+                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                    >
+                        <img src={"src"}/>
+                    </GridTile>
+                    <GridTile
+                        onClick={props.manageEntityAction}
+                        key={"2"}
+                        title={"Crystal tower"}
+                        subtitle={<span>type <b>tower</b></span>}
+                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                    >
+                        <img src={"src"}/>
+                    </GridTile>
+                    <GridTile
+                        key={"3"}
+                        title={"tile.title"}
+                        subtitle={<span>by <b> tile.author </b></span>}
+                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                    >
+                        <img src={"src"}/>
+                    </GridTile>
+                    <GridTile
+                        key={"4"}
+                        title={"tile.title"}
+                        subtitle={<span>by <b> tile.author </b></span>}
+                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                    >
+                        <img src={"src"}/>
+                    </GridTile>
+                    <GridTile
+                        key={"5"}
+                        title={"tile.title"}
+                        subtitle={<span>by <b> tile.author </b></span>}
+                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                    >
+                        <img src={"src"}/>
+                    </GridTile>
+                    <GridTile
+                        key={"6"}
+                        title={"tile.title"}
+                        subtitle={<span>by <b> tile.author </b></span>}
+                        actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                    >
+                        <img src={"src"}/>
+                    </GridTile>
+                </GridList>
+                    <GridList
+                        cellHeight={200}
+                        cols={6}
+                    >
+                        <Subheader>All others</Subheader>
+                        <GridTile
+                            key={"7"}
+                            title={"tile.title"}
+                            subtitle={<span>by <b> tile.author </b></span>}
+                            actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+                        >
+                            <img src={"src"}/>
+                        </GridTile>
+
+                    </GridList>
+                </div>
+            } else {
+                return props.children;
+            }
+        })()}
     </div>
 )
 
--- src/routes/Entity/index.js
+++ src/routes/Entity/index.js
@@ -3,6 +3,38 @@ import EntityView from './components/EntityView'
 
 // Sync route definition
 export default {
-  component: EntityView,
-  path: '/project/:id/entity'
+  getComponent (nextState, cb) {
+    /*  Webpack - use 'require.ensure' to create a split point
+     and embed an async module loader (jsonp) when bundling   */
+    require.ensure([], (require) => {
+      /*  Webpack - use require callback to define
+       dependencies for bundling   */
+      const Home = require('./containers/EntityListContainer').default
+
+      /*  Return getComponent   */
+      cb(null, Home)
+
+      /* Webpack named bundle   */
+    }, 'entity')
+  },
+  path: '/project/:projectId/entity',
+  childRoutes: [
+    {
+      getComponent (nextState, cb) {
+        /*  Webpack - use 'require.ensure' to create a split point
+         and embed an async module loader (jsonp) when bundling   */
+        require.ensure([], (require) => {
+          /*  Webpack - use require callback to define
+           dependencies for bundling   */
+          const Home = require('./containers/EntityManageContainer').default
+
+          /*  Return getComponent   */
+          cb(null, Home)
+
+          /* Webpack named bundle   */
+        }, 'entityManage')
+      },
+      path: '/project/:projectId/entity/:entityId'
+    }
+  ]
 }
